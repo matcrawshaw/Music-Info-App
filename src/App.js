dea5075e93a600837a95deb8dccc9d5861129ad6
matcrawshaw <mat_crawshaw@hotmail.com>
2023-03-24 10:34:53 +0000

    added auth call and get requests for albums and artists, as well as function to retrieve album art with limitation of api being artist pictures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
 import './App.css';
 import NavbarSimple from './components/navbar'
 import CardGrid from './components/cardGrid';
+import { Button } from '@mantine/core';
+import MD5 from "crypto-js/md5"
+
+const token = window.location.search.substring(7);   
+
+if (token) {
+  const hash = MD5("api_keyf8b32377438bdf91d564673f48fba700methodauth.getSessiontoken" + token + "1a7a3c0954170c39e06a9f6c1a5d9358");
+
+  fetch("http://ws.audioscrobbler.com/2.0/?method=auth.getSession&api_key=f8b32377438bdf91d564673f48fba700&token=" + token + "&format=json&api_sig=" + hash)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const currentUsername = data.session.name;
+    const currentUserkey = data.session.key;
+
+    console.log(currentUsername);
+    console.log(currentUserkey);
+  });
+}
+
+fetch("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=f8b32377438bdf91d564673f48fba700&format=json")
+  .then((response) => response.json())
+  .then((data) => {
+    const topTracks = data;
+    console.log("Top Tracks");
+    console.log(topTracks);
+  })
+
+  fetch("http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=f8b32377438bdf91d564673f48fba700&format=json")
+  .then((response) => response.json())
+  .then((data) => {
+    const topArtists = data;
+    console.log("Top Artists");
+    console.log(topArtists);
+  })
+
+  
+const getArtistImage = (artist) => {
+  fetch("http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + artist + "&api_key=f8b32377438bdf91d564673f48fba700&format=json")
+  .then((response) => response.json())
+  .then((data) => {
+return (data.topalbums.album[0].image[1]['#text']) })
+}
+
+
 
 function App() {
+
+
+
+
+getArtistImage("four year strong")
+
   return (
     <div className="App" >
-
+ <Button
+        component="a"
+        rel="noopener noreferrer"
+        href="https://www.last.fm/api/auth?api_key=f8b32377438bdf91d564673f48fba700&cb=http://localhost:3000/" ></Button>
 
 <div style={{display: "flex"}}>
 <NavbarSimple/>
