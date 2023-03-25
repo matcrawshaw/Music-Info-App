96e48d23d25d1c3d6d44062f7347b847f87bc1d4
HllMtrxWrld <119111798+HllMtrxWrld@users.noreply.github.com>
2023-03-25 20:18:51 +0000

    map function take arrays of artists and tracks and creates cards

--- src/components/cardGrid.js
+++ src/components/cardGrid.js
@@ -1,30 +1,91 @@
 import ArticleCardImage from './card';
 import { SimpleGrid } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import Wrapper from './wrapper';
 
-function CardGrid () { 
 
-          return (
+
+
+function CardGrid () {
+
+  const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
+
+  useEffect(() => {
+
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=f8b32377438bdf91d564673f48fba700&format=json`)
+  .then((response) => response.json())
+  .then((data) => {
+    setTopArtists(data.artists.artist);
+    console.log("Top Artists");
+    console.log(topArtists);
+  });
+ }, []);
+
+ useEffect(() => {
+
+  fetch("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=f8b32377438bdf91d564673f48fba700&format=json")
+  .then((response) => response.json())
+  .then((data) => {
+    setTopTracks(data.tracks.track);
+    console.log("Top Tracks");
+    console.log(topTracks);
+  });
+ }, []);
+
+ const renderArtists = function () {
+  return (
+    topArtists.map((artist) => {
+      return (
+    <ArticleCardImage 
+    title={artist.name} 
+    category="rock" 
+    image="https://png.pngtree.com/png-clipart/20190517/original/pngtree-rock-group-music-band-png-image_3621390.jpg"/>
+    ); 
+  })
+  )
+};
+
+
+const renderTracks = function () {
+  return (
+    topTracks.map((track) => {
+      return (
+    <ArticleCardImage 
+    title={track.name} 
+    category="rock" 
+    image="https://png.pngtree.com/png-clipart/20190517/original/pngtree-rock-group-music-band-png-image_3621390.jpg"/>
+      ); 
+  })
+  )
+};
+
+
+
+  if (topArtists) {
+  return (
             <SimpleGrid style={{display: "flex", flexDirection: "column"}} cols={1}>
               <h2 style={{display: "flex", justifyContent: "start", marginLeft: 100}}>Top Artists</h2>
-              <div style={{display: "flex", flexDirection: "row", paddingLeft: 100}} cols={1}>
-              <div>
+              <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap", marginLeft: 100}} cols={1}>
+              {renderArtists()};
+              </div>
+              <h2 style={{display: "flex", justifyContent: "start", marginLeft: 100}}>Top Tracks</h2>
+              <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap", marginLeft: 100}} cols={1}>
+              {renderTracks()};
+            </div>
+              {/* <div style={{marginLeft: 20}}>
               <ArticleCardImage title="new band here" category="rock" image="https://png.pngtree.com/png-clipart/20190517/original/pngtree-rock-group-music-band-png-image_3621390.jpg"/>
               </div>
-              <div style={{marginLeft: 20}}>
-              <ArticleCardImage title="new band here" category="rock" image="https://png.pngtree.com/png-clipart/20190517/original/pngtree-rock-group-music-band-png-image_3621390.jpg"/>
-              </div>
-              <div style={{marginLeft: 20}}>
-              <ArticleCardImage title="new band here" category="rock" image="https://png.pngtree.com/png-clipart/20190517/original/pngtree-rock-group-music-band-png-image_3621390.jpg"/>
-              </div>
-              <div style={{marginLeft: 20}}>
-              <ArticleCardImage title="new band here" category="rock" image="https://png.pngtree.com/png-clipart/20190517/original/pngtree-rock-group-music-band-png-image_3621390.jpg"/>
-              </div>
-              <div style={{marginLeft: 20}}>
-                <ArticleCardImage title="new band here" category="rock" image="https://png.pngtree.com/png-clipart/20190517/original/pngtree-rock-group-music-band-png-image_3621390.jpg"/>
-              </div>
-              </div>
-            </SimpleGrid>
+               */}
+          </SimpleGrid> 
+      
           )
+          } 
         }
+  
 
    export default CardGrid;
+
+
+  
+
