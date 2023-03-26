0114d22d7af7b1c12c0adfda528495bc0a618d96
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-03-26 02:38:17 +0100

    adding corrections to login.js. It does not transfer from login to signup page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -11,16 +11,17 @@ function Login() {
 
   //Store in localStorage
   let storedItems = [];
+  let itemsInStore = [];
   storedItems = JSON.parse(localStorage.getItem(itemsInStore));
-  if (storedItems !==null) {
-    const handleSubmit = (e) => {
-      storedItems.forEach((item) => {
-        if (item.name === input.name && item.email === input.email && item.password === input.password) {
-          alert(`Hello, ${input.name}, welcome to Music Info App`);
-        }
-      });
-    }
+ 
+  const handleSubmit = (e) => {
+    storedItems.forEach((item) => {
+      if (item.name === input.name && item.email === input.email && item.password === input.password) {
+        alert(`Hello, ${input.name}, welcome to Music Info App`);
+      }
+    });
   }
+  
 
   return (
     <div className="log">
@@ -34,12 +35,12 @@ function Login() {
         <div className="right">
           <div className="box">
             <form onSubmit={handleSubmit}>
-            <input name="name" value={input.name} onChange={(e) => setInput({ ... input, [e.target.name] : e.target.value,})} type="text" id="form3Examplelcg" placeholder="Enter a Username" className="input form-control-lg" />
-            <input name="email" value={input.email} onChange={(e) => setInput({ ... input, [e.target.email] : e.target.value,})} type="email" id="form3Examplelcg" placeholder="Enter a email" className="input" />
-            <input name="password" value={input.password} onChange={(e) => setInput({ ... input, [e.target.password] : e.target.value,})} type="password" id="form3Examplelcg" placeholder="Enter a Password" className="input" />
-            <button type="submit" className="signupButton">Log into Account</button>
-            <button type="submit" className="logButton">Sign Up</button>
+            <input name="name" value={input.name} onChange={(e) => setInput({ ...input, [e.target.name] : e.target.value,})} type="text" id="form3Examplelcg" placeholder="Enter a Username" className="input form-control-lg" />
+            <input name="email" value={input.email} onChange={(e) => setInput({ ...input, [e.target.email] : e.target.value,})} type="email" id="form3Examplelcg" placeholder="Enter a email" className="input" />
+            <input name="password" value={input.password} onChange={(e) => setInput({ ...input, [e.target.password] : e.target.value,})} type="password" id="form3Examplelcg" placeholder="Enter a Password" className="input" />
+            <button type="submit" className="logButton">Log into Account</button>
             </form>
+            <a href="../login"><button type="button" className="signupButton">SignUp</button></a>
           </div>
         </div>
       </div>
