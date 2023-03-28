50f31228dcc6bc549170b1efe9091d8d7d9e93b1
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-03-28 13:06:37 +0100

    Adding function to display results of search

--- src/components/DisplayArtist.js
+++ src/components/DisplayArtist.js
@@ -1,30 +1,33 @@
+import ArtistSearchCard from "./ArtistSearchCard/ArtistSearchCard";
+
 const DisplayArtist = ({infoBio = [], infoTAlbums = []}) => {
+  if (infoBio)
   //console.log("data-DisplayArtist-infoBio:", infoBio);
   console.log("data-DisplayArtist-infoTAlbums:", infoTAlbums);
-  //console.log("----------------------fetchArtist-data1a:", infoBio.artist.bio.content);
-  //console.log("----------------------fetchArtist-data1b:", infoBio.artist.bio.links.link.href);
-  //console.log("----------------------fetchArtist-data1c:", infoTAlbums.topalbums.album[0].image[2]["#text"]);
-  console.log("----------------------fetchArtist-data1d:", infoTAlbums.topalbums.album[0].name);
+  //console.log("----------------------DisplayArtist-data1a:", infoBio.artist.bio.content);
+  console.log("----------------------DisplayArtist-data1b:", infoBio.artist.bio.links.link.href);
+  //console.log("----------------------DisplayArtist-data1c:", infoTAlbums.topalbums.album[0].image[2]["#text"]);
+  //console.log("----------------------DisplayArtist-data1d:", infoTAlbums.topalbums.album[0].name);
   let artist = [];
-  for (let i = 0; i < infoTAlbums.length; i++) {
-      artist.push({nameAlbum: infoTAlbums.topalbums.album[i].name, imageAlbum: infoTAlbums.topalbums.album[i].image[2]["#text"]});
+  for (let i = 0; i < infoTAlbums.topalbums.album.length; i++) {
+      artist.push({id: i, nameAlbum: infoTAlbums.topalbums.album[i].name, imageAlbum: infoTAlbums.topalbums.album[i].image[2]["#text"]});
   }
   console.log("----------------------DisplayArtist-Artist:", artist);
 
-  /*(    
+  /**/
+    //Display the data on screen. 
+    return (    
       <div className="artist-wrapper">
+        <h1>Biography</h1>
+        <p>{infoBio.artist.bio.content}</p>
         {infoTAlbums && 
-        infoTAlbums.album.title.map((title) => (
-          title &&
+        artist.map((artAlb) => (
             <div className='artist-list'>
-              <img src={infoTAlbums.album.cover} alt="Album Pictures" />
-              <h4>{title}</h4>
+              <ArtistSearchCard id={artAlb.id} album={artist.nameAlbum} image={artist.imageAlbum} />
             </div>                        
           ))}
       </div>
-    );*/
-    //Display the data on screen. 
-    return    
+    );
   };
   
   export default DisplayArtist;
