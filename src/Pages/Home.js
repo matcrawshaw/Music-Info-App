fcc12e3d2bb13a7b3577c73f9e41ac83d0bf8258
matcrawshaw <mat_crawshaw@hotmail.com>
2023-03-29 15:28:26 +0100

    distanced grid from nav on home page slightly

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -29,7 +29,7 @@ return GuestGreeting()
 
 
 return (
-<div style={{marginLeft: 150, flexWrap: "wrap", whiteSpace: "pre-wrap"}}>
+<div style={{marginLeft: 170, flexWrap: "wrap", whiteSpace: "pre-wrap"}}>
 <div style={{display: "flex",justifyContent: "flex-start"}}>
 {/* <Greeting/> */}
 </div>
